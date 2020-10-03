591c8ab084eb02b118b5ecf9d0f8a9a9a4fa998c
itcast <2592821355@qq.com>
2020-10-03 08:30:08 +0800

    sss

--- src/main/java/com/itheima/controller/SsMaterialsController.java
+++ src/main/java/com/itheima/controller/SsMaterialsController.java
@@ -8,7 +8,7 @@ public class SsMaterialsController {
 
     @GetMapping(value = "/SsFindAll")
     public String SsFindAll() {
-
+        System.out.println("123");
         return "SsFindAll";
     }
 }
